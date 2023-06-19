5d7524dd72ffc8913a91030f106e9128ac78367f
Ryan Phan <ryanwithcodes@gmail.com>
2023-06-19 08:56:34 +0700

    ref: :bulb: - update get user follow using `criteria query`

--- user/src/main/java/com/ryan/domain/dao/implement/UserFollowDaoImpl.java
+++ user/src/main/java/com/ryan/domain/dao/implement/UserFollowDaoImpl.java
@@ -14,39 +14,37 @@ import org.springframework.stereotype.Repository;
 @AllArgsConstructor
 public class UserFollowDaoImpl implements UserFollowDao {
 
-  private EntityManager entityManager;
+    private EntityManager entityManager;
 
-  @Override
-  public UserFollowDetail getUserFollowDetail(Long userId) {
-    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-    CriteriaQuery<Tuple> query = builder.createTupleQuery();
-    Root<UserFollow> root = query.from(UserFollow.class);
+    @Override
+    public UserFollowDetail getUserFollowDetail(Long userId) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> query = builder.createTupleQuery();
+        Root<UserFollow> root = query.from(UserFollow.class);
 
-    Predicate fromUserCondition = builder.equal(root.get("fromUserId"), userId);
-    Predicate toUserCondition = builder.equal(root.get("toUserId"), userId);
-    Predicate whereCondition = builder.or(fromUserCondition, toUserCondition);
+        Predicate fromUserCondition = builder.equal(root.get("fromUserId"), userId);
+        Predicate toUserCondition = builder.equal(root.get("toUserId"), userId);
+        Predicate whereCondition = builder.or(fromUserCondition, toUserCondition);
 
-    var fromUserSelect = builder.selectCase()
-            .when(fromUserCondition, 1);
-    var toUserSelect = builder.selectCase()
-            .when(toUserCondition, 1);
+        var fromUserSelect = builder.selectCase()
+                .when(fromUserCondition, 1);
+        var toUserSelect = builder.selectCase()
+                .when(toUserCondition, 1);
 
-    Expression<Long> fromUserCountEx = builder.count(fromUserSelect);
-    Expression<Long> toUserCountEx = builder.count(toUserSelect);
+        Expression<Long> fromUserCountEx = builder.count(fromUserSelect);
+        Expression<Long> toUserCountEx = builder.count(toUserSelect);
 
-    query.select(builder.tuple(
-            fromUserCountEx.alias("from_user_count"),
-            toUserCountEx.alias("to_user_count")
-    )).where(whereCondition);
+        query.select(builder.tuple(
+                fromUserCountEx.alias("from_user_count"),
+                toUserCountEx.alias("to_user_count")
+        )).where(whereCondition);
 
-    TypedQuery<Tuple> typedQuery = entityManager.createQuery(query);
-    Tuple result = typedQuery.getSingleResult();
+        TypedQuery<Tuple> typedQuery = entityManager.createQuery(query);
+        Tuple result = typedQuery.getSingleResult();
 
-    Long fromUserCount = result.get("from_user_count", Long.class);
-    Long toUserCount = result.get("to_user_count", Long.class);
-    return UserFollowDetail.builder()
-            .followers(toUserCount)
-            .following(fromUserCount)
-            .build();
-  }
+        return UserFollowDetail.builder()
+                .followers(result.get("to_user_count", Long.class))
+                .following(result.get("from_user_count", Long.class))
+                .build();
+    }
 }
